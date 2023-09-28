1d615924e8b37b460f553f73d6e3e5625a95538a
lorenzo-neri <lorenzo21neri@gmail.com>
2023-09-28 16:09:10 +0200

    add @keyup.enter on input

--- src/App.vue
+++ src/App.vue
@@ -29,14 +29,17 @@ export default {
 </script>
 
 <template>
-  <div class="container">
+  <div class="container p-3">
+
+    <h1 class="text-danger text-center">BOOLFIX</h1>
+
     <div class="row p-2">
       <div class="col">
 
         <div class="input-group mb-3">
-          <input name="searchBar" id="searchBar" v-model="this.store.searchQuery" type="search" class="form-control"
-            placeholder="Cerca film per nome" />
-          <button @click="searchMovies" class="btn btn-primary">
+          <input @keyup.enter="searchMovies" name="searchBar" id="searchBar" v-model="this.store.searchQuery"
+            type="search" class="form-control border-0" placeholder="Cerca film per nome" />
+          <button @click="searchMovies" class="btn btn-danger">
             Cerca
           </button>
         </div>
@@ -47,15 +50,21 @@ export default {
     </div>
     <!-- /.row -->
 
-    <div class="row">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-6">
 
-      <div class="col" v-for="(movie, index) in store.movies" :key="index">
+      <div class="col p-3" v-for="(movie, index) in  store.movies " :key="index">
 
         <div class="card">
           <div class="card-body">
             <h5 class="card-title">{{ movie.title }}</h5>
             <p class="card-text">Titolo Originale: {{ movie.original_title }}</p>
-            <p class="card-text">Lingua: {{ movie.original_language }}</p>
+            <p class="card-text">
+              <span v-if="movie.original_language == 'en'">
+                <img :src="'https://icons8.it/icon/t3NE3BsOAQwq/great-britain'" :alt="movie.original_language">
+              </span>
+              <span v-else> Lingua: {{ movie.original_language }}</span>
+            </p>
+
             <p class="card-text">Voto: {{ movie.vote_average }}</p>
           </div>
         </div>
@@ -71,4 +80,8 @@ export default {
   <!-- /.container -->
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+img {
+  width: 100%;
+}
+</style>
