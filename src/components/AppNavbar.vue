d6492e5b7596088cffd095d84fe79ec8ef889a8c
lorenzo-neri <lorenzo21neri@gmail.com>
2023-09-29 18:41:25 +0200

    add AppNavbar clean

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,41 @@
+<script>
+import { store } from './store.js';
+
+export default {
+
+    name: 'AppNavbar',
+    components: {
+
+    },
+
+    data() {
+        return {
+            store,
+        }
+    },
+
+    methods: {
+        searchMovies() {
+            store.results = [];
+            store.fetchMovies(this.store.userSearch);
+            store.fetchTvs(this.store.userSearch);
+            store.userSearch = ''
+        },
+
+        voteOneToFive(vote) {
+            const roundedVote = Math.ceil(vote);
+            const voteOnetoFive = roundedVote / 2;
+            const roundedVoteOneToFive = Math.ceil(voteOnetoFive);
+            return Number(roundedVoteOneToFive)
+        }
+    },
+
+    created() {
+    },
+
+}
+</script>
+
+<template></template>
+
+<style lang="scss" scoped></style>
